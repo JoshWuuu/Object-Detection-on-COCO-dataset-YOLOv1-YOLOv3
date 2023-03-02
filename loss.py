6dab51f8c73966c237a4ac5c119a5d6e9bf23be1
ChunLiang Wu <lucky0978218035@gmail.com>
2023-03-01 22:05:22 -0800

    Create loss.py

---
+++ loss.py
@@ -0,0 +1,47 @@
+import torch 
+import torch.nn as nn
+from metrics import intersection_over_union
+
+class YoLoLoss(nn.Module):
+    """YOLO Loss function, calculates the loss for all the cells in the grid for the YOLOv1 model."""
+    def __init__(self, S=7, B=2, C=20) -> None:
+        super(YoloLoss, self).__init__()
+        self.mse = nn.MSELoss(reduction="sum")
+        self.S = S
+        self.B = B
+        self.C = C
+        self.lambda_noobj = 0.5
+        self.lambda_coord = 5
+
+    def forward(self, predictions, target):
+        predictions = predictions.reshape(-1, self.S, self.S, self.C + self.B * 5)
+        iou_b1 = intersection_over_union(predictions[..., 21:25], target[..., 21:25])
+        iou_b2 = intersection_over_union(predictions[..., 26:30], target[..., 21:25])
+        ious = torch.cat([iou_b1.unsqueeze(0), iou_b2.unsqueeze(0)], dim=0)
+        iou_maxes, bestbox = torch.max(ious, dim=0) # bestbox is the index of the best box, 0 or 1
+        exists_box = target[..., 20].unsqueeze(3)  # 1 if there is an object in the cell, 0 otherwise
+
+        # Loss for box coordinates
+        box_predictions = exists_box * (
+            # determine which box is the best box
+            (
+                bestbox * predictions[..., 26:30]
+                + (1 - bestbox) * predictions[..., 21:25]
+            )
+        )
+
+        box_targets = exists_box * target[..., 21:25]
+
+        # width and height of the box
+        box_predictions[..., 2:4] = torch.sign(box_predictions[..., 2:4]) * torch.sqrt(
+            torch.abs(box_predictions[..., 2:4] + 1e-6))
+        box_targets[..., 2:4] = torch.sqrt(box_targets[..., 2:4])
+
+        # (N, S, S, 4) -> (N*S*S, 4)
+        box_loss = self.mse(
+            torch.flatten(box_predictions, end_dim=-2),
+
+
+
+
+
